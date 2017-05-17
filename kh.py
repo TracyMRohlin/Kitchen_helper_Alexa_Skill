3f7b9173a3e066127bbc44059efb18e663778873
tracy rohlin <tracy.rohlin@gmail.com>
2017-05-16 22:29:22 -0700

    Add files via upload

--- kh.py
+++ kh.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_ask import Ask, statement, question, convert_errors
 
@@ -32,7 +33,11 @@ def dec_to_str(total):
         return "0:3f".format(total)
 
 def str_to_dec(string):
-    if string == "one eighth":
+    if string == None:
+        return 0
+    elif string == "a":
+        return 1
+    elif string == "one eighth":
         return 0.125
     elif string == "one quarter":
         return 0.25
@@ -46,7 +51,7 @@ def str_to_dec(string):
         except:
             raise Exception("Unable to process that number")
 
-@ask.intent('ImperialIntent')
+#@ask.intent('ImperialIntent')
 def convert_imperial(amount, from_unit, to_unit):
     conversions = {"g":{"q":4, "p": 8, "c":16, "o":128, "t":256},
                    "q":{"g":0.25, "p":2, "c": 4, "o":32, "t":64},
@@ -55,7 +60,7 @@ def convert_imperial(amount, from_unit, to_unit):
                    "o":{"g":0.0078125, "q":0.03125, "c":0.125, "t":2},
                    "t":{"o": 0.5}
                    }
-    quantity = str_to_dec(amount)
+    quantity = str_to_dec(whole_num) + str_to_dec(fraction)
     if from_unit in conversions and to_unit in conversions[to_unit]:
         total = quantity * conversions[from_unit[0]][to_unit[0]]
         total = dec_to_str(round(total * 8) / 8)
@@ -76,7 +81,7 @@ def convert_imperial(amount, from_unit, to_unit):
 # TODO Ingredient substitutions
 
 
-@ask.intent('JuiceIntent', convert={"num": int})
+@ask.intent('JuiceIntent', convert={"num": int}, default={"num":1})
 def juice(fruit, num):
     if fruit == "lemons" or fruit == "lemon":
         factor = 3
@@ -90,27 +95,28 @@ def juice(fruit, num):
     speech_text = "There are {0} tablespoons or {1} teaspoons of juice in {2} {3}".format(tbs, tsps, num, fruit)
     return statement(speech_text)
 
-@ask.intent('ZestIntent', convert={"num": int})
-def zest(fruit, num)
-    if fruit == "lemons" or fruit == "lemon":
+@ask.intent('ZestIntent', convert={"num": int}, default={"num_fruit":1})
+def zest(fruit_type, num_fruit):
+    if fruit_type == "lemons" or fruit_type == "lemon":
         factor = 3
-    elif fruit == "limes" or fruit == "lime":
+    elif fruit_type == "limes" or fruit_type == "lime":
         factor = 2
     else:
         factor = 6
 
-    tsps = factor * num
-    tbs = dec_to_str(convert_imperial(tsps, "table", "tea")
-    speech_text = "There are {0} teaspoons or {1} tablespoons of zest in {2} {3}".format(tsps, tbs, num, fruit)
+    tsps = factor * num_fruit
+    tbs = dec_to_str(convert_imperial(tsps, "table", "tea"))
+    speech_text = "There are {0} teaspoons or {1} tablespoons of zest in {2} {3}".format(tsps, tbs, num_fruit, fruit)
     return statement(speech_text)
 
     # dried to fresh herb conversion
 
-@ask.intent("HerbIntent", convert={"num": int})
+@ask.intent("HerbIntent", default={'num': 'a'})
 def herb(num, orig_unit):
     verb = ""
     unit = ""
     total = 0
+    num = str_to_dec(num)
     if orig_unit == "cup" or "cups":                                 # if asking for dried in a cup
         tbs = 16 * num
         total = tbs * 1/3.0
@@ -154,7 +160,18 @@ def stop():
 def cancel():
     return statement("Goodbye")
 
+@ask.intent('AMAZON.HelpIntent')
+def stop():
+    return statement("This app helps you in the kitchen as you cook.")
 
 @ask.session_ended
 def session_ended():
     return "{}", 200
+
+
+if __name__ == '__main__':
+    if 'ASK_VERIFY_REQUESTS' in os.environ:
+        verify = str(os.environ.get('ASK_VERIFY_REQUESTS', '')).lower()
+        if verify == 'false':
+            app.config['ASK_VERIFY_REQUESTS'] = False
+    app.run(debug=True)
